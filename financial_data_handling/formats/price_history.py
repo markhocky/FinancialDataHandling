1cd2158afb8d8014b3c1f23f6563e2361f7d10ac
Mark Hochguertel <markhocky@gmail.com>
2017-11-05 14:27:07 +0800

    Added some utility methods to Instruments to help with things like subsetting on tickers.

--- financial_data_handling/formats/price_history.py
+++ financial_data_handling/formats/price_history.py
@@ -8,6 +8,9 @@ class Instruments(StorageResource):
 
     def __init__(self, name):
         self.name = name
+        self.data = None
+        self.start = None
+        self.end = None
         
     def select_folder(self, store):
         self.exchange = store.exchange
@@ -30,10 +33,43 @@ class Instruments(StorageResource):
         return list(self.data.items)
 
     def exclude(self, excluded_tickers):
+        '''
+        Removes the specified tickers from instrument set.
+        '''
         tickers = list(set(self.tickers) - set(excluded_tickers))
-        self.data = self.data.loc[tickers, :, :]
+        new_set = Instruments(self.name)
+        new_set.exchange = self.exchange
+        new_set.start = self.start
+        new_set.end = self.end
+        new_set.data = self.data.loc[tickers, :, :]
+        return new_set
+
+    def include_only(self, included_tickers):
+        '''
+        Removes tickers which are not in the provided ticker set
+        '''
+        tickers = list(set(included_tickers).intersection(set(self.tickers)))
+        new_set = Instruments(self.name)
+        new_set.exchange = self.exchange
+        new_set.start = self.start
+        new_set.end = self.end
+        new_set.data = self.data.loc[tickers, :, :]
+        return new_set
+
+    def up_to(self, end_date):
+        '''
+        Returns a new instruments object with a revised (shorter) end date.
+        '''
+        new_set = Instruments(self.name)
+        new_set.exchange = self.exchange
+        new_set.start = self.start
+        new_set.data = self.data.loc[:, :, :end_date]
+        new_set.end = self.data.iloc[0].index.max().to_pydatetime().date()
+        return new_set
 
 
+# TODO - provide methods for updating the data (i.e. for removing errors).
+# TODO - consider adding a merge method for price history to append new data rather than overwrite.
 class PriceHistory(StorageResource):
     
     def __init__(self, ticker):
